cd0f594f61bf550d8025074bf15e642fa8c12259
oxmmty <oxmmty@gmail.com>
2024-09-19 08:52:42 +0900

    change state.rs file

--- src/state.rs
+++ src/state.rs
@@ -14,7 +14,7 @@ pub enum GameStatus {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct GlobalState {
     // pub count: u64,
-    // pub owner: Addr
+    pub owner: Addr
 }
 
 
@@ -22,16 +22,16 @@ pub struct GlobalState {
 pub struct GameState {
     pub ticket_price: u64,
     pub sold_ticket_count: u64,
-    // pub total_ticket_count: u64,
+    pub total_ticket_count: u64,
     pub raffle_status: u8,
-    // pub nft_contract_addr: Addr,
+    pub nft_contract_addr: Addr,
     pub nft_token_id: String,
-    // pub owner: Addr,
+    pub owner: Addr,
     pub collection_wallet: Addr, // Collection wallet address to send tokens after the game finished
-    // pub end_time: u64,
+    pub end_time: u64,
 }
 
-// pub const GLOBAL_STATE: Item<GlobalState> = Item::new("global_state");
-// pub const GAME_STATE: Map<u64, GameState> = Map::new("game_state");
-// pub const TICKET_STATUS: Map<(u64, u64), Addr> = Map::new("ticket_status");
-// pub const WALLET_TICKETS: Map<(u64, Addr), Vec<u64>> = Map::new("wallet_tickets");
+pub const GLOBAL_STATE: Item<GlobalState> = Item::new("global_state");
+pub const GAME_STATE: Map<u64, GameState> = Map::new("game_state");
+pub const TICKET_STATUS: Map<(u64, u64), Addr> = Map::new("ticket_status");
+pub const WALLET_TICKETS: Map<(u64, Addr), Vec<u64>> = Map::new("wallet_tickets");
