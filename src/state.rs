3fdb40436109ce8dba930f975657e6eccc65b033
oxmmty <oxmmty@gmail.com>
2024-09-19 09:06:14 +0900

    change cw_storage_plus

--- src/state.rs
+++ src/state.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 
 // use cosmwasm_std::Addr;
-// use cw_storage_plus::{Item, Map};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub enum GameStatus {
