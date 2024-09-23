72eabf567f7768283793f419fe6e8190c0bb6ff1
oxmmty <oxmmty@gmail.com>
2024-09-23 22:26:28 +0900

    change InstantiateMsg

--- src/contract.rs
+++ src/contract.rs
@@ -25,7 +25,7 @@ pub fn instantiate(
     // msg: InstantiateMsg,
 
     // info: MessageInfo,
-    msg: InstantiateMsg,
+    // msg: InstantiateMsg,
 
 ) -> Result<Response, ContractError> {
     let sender_str = info.sender.clone().to_string();
