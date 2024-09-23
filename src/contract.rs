ae5d656ea98b2723ea7b9d4262b82167052d2e93
OxMMty <oxmmty@gmail.com>
2024-09-23 22:30:47 +0900

    Dev (#28)
    * fix
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * fix
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * fix
    
    Co-authored-by: oxmmty <oxmmty@gmail.com>
    
    * change state.rs file
    
    * change TimeOver
    
    * change cw_storage_plus
    
    * change cw_stoage_plus
    
    * change DepsMut parameter
    
    * changed InstantiateMsg
    
    * change MessageInfo
    
    * change InstantiateMsg
    
    * change try_receive_nft funcion

--- src/contract.rs
+++ src/contract.rs
@@ -18,18 +18,10 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    // deps: DepsMut,
-    // _env: Env,
-
-    // info: MessageInfo,
-    // msg: InstantiateMsg,
-
-    // info: MessageInfo,
-
-    // msg: InstantiateMsg,
-
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
     msg: InstantiateMsg,
-
 
 ) -> Result<Response, ContractError> {
     let sender_str = info.sender.clone().to_string();
